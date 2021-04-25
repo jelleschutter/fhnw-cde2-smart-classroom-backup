3befdfe18ec0d943f31bff2e473a7a847cefe90e
Jelle Schutter <jelle@schutter.xyz>
2021-04-26 00:06:04 +0200

    generate backup via sql

--- helper.py
+++ helper.py
@@ -1,10 +1,9 @@
 import getopt
 import sys
-import requests
 from pathlib import Path
 
 def get_opts():
-    aopts = getopt.getopt(sys.argv[1:], '', ['username=', 'password=', 'endpoint='])
+    aopts = getopt.getopt(sys.argv[1:], '', ['username=', 'password=', 'hostname=', 'port=', 'service='])
 
     opts = {}
     for opt in aopts[0]:
@@ -13,21 +12,6 @@ def get_opts():
         else:
             opts[opt[0]] = opt[1]
     return opts
-
-def get_all_items(url, auth=None):
-    current_url = url
-    items = []
-    hasMore = True
-    offset = 0
-    while hasMore:
-        response = requests.get(current_url, auth=auth)
-        content = response.json()
-        items = [*items, *content['items']]
-        hasMore = content['hasMore']
-        if hasMore:
-            offset += content['count']
-            current_url = url + '?offset=' + str(offset)
-    return items
 
 def ensure_dir(path):
     Path(path).mkdir(parents=True, exist_ok=True)
--- main.py
+++ main.py
@@ -1,39 +1,44 @@
-import pandas as pd
 import getopt
 import sys
-from helper import get_all_items, get_opts, ensure_dir
+import pandas as pd
+from helper import get_opts, ensure_dir
+import cx_Oracle
 
 opts = get_opts()
 
 ensure_dir('./data/measurements')
 
+print('Connecting to DB...')
+
+connection = cx_Oracle.connect(opts['username'], opts['password'], f"tcps://{opts['hostname']}:{opts['port']}/{opts['service']}?wallet_location=./secrets/Oracle_Wallet&retry_count=20&retry_delay=3")
+
+print('Connected to DB.')
+
 print('Loading sensors...')
 
-sensors = get_all_items(opts['endpoint'] + '/sensors', auth=(opts['username'], opts['password']))
+sensors = pd.read_sql('SELECT * FROM sensor_meta_infos', connection)
 
 print('Finished loading sensors.')
 
 print('Saving sensors to CSV...')
 
-sensors_df = pd.DataFrame(sensors)
-sensors_df.to_csv('./data/sensors.csv', index=False)
+sensors.to_csv('./data/sensors.csv', index=False)
 
 print('Sensors saved to CSV.')
 
 print('Loading measurements...')
 
-measurements = get_all_items(opts['endpoint'] + '/measurements', auth=(opts['username'], opts['password']))
+measurements = pd.read_sql('SELECT * FROM sensor_measurements', connection)
 
 print('Finished loading measurements.')
 
 print('Saving measurements to CSV...')
 
-data_df = pd.DataFrame(measurements)
-data_df['day'] = pd.to_datetime(data_df['insert_timestamp']).dt.floor('d')
+measurements['day'] = pd.to_datetime(measurements['INSERT_TIMESTAMP']).dt.floor('d')
 
-for name, group in data_df.groupby('day'):
+for name, group in measurements.groupby('day'):
     date = name.date()
-    group.drop('day',axis=1).to_csv('./data/measurements/{}.csv'.format(date), index=False)
+    group.drop('day', axis=1).to_csv('./data/measurements/{}.csv'.format(date), index=False)
     print('Saved measurements for {} to CSV.'.format(date))
 
 print('All measurements saved to CSV.')
